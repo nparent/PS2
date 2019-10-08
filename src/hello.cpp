e8cd37b78ae837984f6b912a23e8d6a54319371c
nparent <nicolas.parent.msiam@gmail.com>
2019-10-08 16:54:55 +0200

    need to change the components in find_package boost..

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,48 +1,36 @@
 #include <iostream>
 
+#include <Eigen/Dense>
 #include <boost/signals.hpp>
 #include <boost/timer/timer.hpp>
-#include <Eigen/Dense>
-using namespace Eigen ;
-using namespace boost::time
+using namespace Eigen;
+using namespace boost::timer;
 #include <boost/numeric/ublas/matrix.hpp>
 using namespace boost::numeric::ublas;
-
 
 struct HelloWorld
 
 {
 
-        void operator()() const
+  void operator()() const
 
-        {
+  {
 
-                std::cout << "Hello, World!" << std::endl;
-
-        }
-
+    std::cout << "Hello, World!" << std::endl;
+  }
 };
 
-
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 
 {
 
-        boost::signal0<void> sig;
+  boost::signal0<void> sig;
 
+  HelloWorld hello;
 
+  sig.connect(hello);
 
-        HelloWorld hello;
+  sig();
 
-        sig.connect(hello);
-
-
-
-        sig();
-
-
-
-        return 0;
-
+  return 0;
 }
