f950769d408ba4d111f18606f74ddde2670f74f6
nicolasparentMSIAM <nicolas.parent.MSIAM@gmail.com>
2019-10-20 16:10:16 +0200

    added tools for Boost

--- tests/test_boost_Mxv.cpp
+++ tests/test_boost_Mxv.cpp
@@ -7,41 +7,12 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
 
-// #include <boost/numeric/ublas/assignment.hpp>
-// #include <boost/numeric/ublas/detail/matrix_assign.hpp>
-// #include <boost/numeric/ublas/matrix_proxy.hpp>
+#include "tools.cpp"
 
 using namespace std;
 using namespace Eigen;
 using namespace boost::numeric::ublas;
 
-const matrix<double> CreateRandomMatrix(const std::size_t n_rows, const std::size_t n_cols)
-{
-  boost::numeric::ublas::matrix<double> m(n_rows,n_cols);
-  for (std::size_t row=0; row!=n_rows; ++row)
-  {
-    for (std::size_t col=0; col!=n_cols; ++col)
-    {
-      m(row,col) = static_cast<double>(std::rand()) / static_cast<double>(RAND_MAX);
-    }
-  }
-  return m;
-}
-
-matrix<double> MatrixFromEigen(MatrixXd M)
-{
-  int n_rows = M.rows();
-  int n_cols = M.cols();
-  boost::numeric::ublas::matrix<double> m(n_rows,n_cols);
-  for (std::size_t row=0; row!=n_rows; ++row)
-  {
-    for (std::size_t col=0; col!=n_cols; ++col)
-    {
-      m(row,col) = M(row,col);
-    }
-  }
-  return m;
-}
 
 int main(int argc, char *argv[]) {
   // zero_matrix<int> M(10,10);
---
+++ tests/tools.cpp
@@ -0,0 +1,44 @@
+#ifned TOOLS
+#define TOOLS
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <boost/timer/timer.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+
+using namespace std;
+using namespace Eigen;
+using namespace boost::numeric::ublas;
+
+const matrix<double> CreateRandomMatrix(const std::size_t n_rows, const std::size_t n_cols)
+{
+  boost::numeric::ublas::matrix<double> m(n_rows,n_cols);
+  for (std::size_t row=0; row!=n_rows; ++row)
+  {
+    for (std::size_t col=0; col!=n_cols; ++col)
+    {
+      m(row,col) = static_cast<double>(std::rand()) / static_cast<double>(RAND_MAX);
+    }
+  }
+  return m;
+}
+
+matrix<double> MatrixFromEigen(MatrixXd M)
+{
+  int n_rows = M.rows();
+  int n_cols = M.cols();
+  boost::numeric::ublas::matrix<double> m(n_rows,n_cols);
+  for (std::size_t row=0; row!=n_rows; ++row)
+  {
+    for (std::size_t col=0; col!=n_cols; ++col)
+    {
+      m(row,col) = M(row,col);
+    }
+  }
+  return m;
+}
+
+#endif
